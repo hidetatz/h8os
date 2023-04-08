2a8b0474ab746d6b499b7fc4695017bd21693473
Hidetatz Yaginuma <hidetatz@gmail.com>
2023-04-08 23:23:42 +0900

    add utilities

--- lib.h
+++ lib.h
@@ -1,7 +1,26 @@
 #ifndef H8OS_LIB_H_
 #define H8OS_LIB_H_
 
+/*
+ * for serial
+ */
+
+// put a charactor
 int putc(unsigned char c);
+// put a string
 int puts(unsigned char *str);
+
+/*
+ * utilities
+ */
+
+void *memset(void *b, int c, long len);
+void *memcpy(void *dst, const void *src, long len);
+int memcmp(const void *b1, const void *b2, long len);
+int strlen(const char *s);
+char *strcpy(char *dst, const char *src);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, int len);
+int putxval(unsigned long value, int column);
 
 #endif // H8OS_LIB_H_
