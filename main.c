7f679cb975c6bad497160349908b66ff67b36e00
Hidetatz Yaginuma <hidetatz@gmail.com>
2023-04-09 22:50:11 +0900

    load any program on boot by command

--- main.c
+++ main.c
@@ -1,18 +1,11 @@
+/*
+ * h8os bootloader.
+ */
+
 #include "defines.h"
 #include "serial.h"
+#include "xmodem.h"
 #include "lib.h"
-
-int global_data = 0x10;
-int global_bss;
-static int static_data = 0x20;
-static int static_bss;
-
-static void printval(void) {
-	puts("global_data = "); putxval(global_data, 0); puts("\n");
-	puts("global_bss  = "); putxval(global_bss,  0); puts("\n");
-	puts("static_data = "); putxval(static_data, 0); puts("\n");
-	puts("static_bss  = "); putxval(static_bss,  0); puts("\n");
-}
 
 static int init(void) {
 	// defined in linker script
@@ -34,20 +27,67 @@ static int init(void) {
 	return 0;
 }
 
-int main(void) {
-	init();
-	puts("Hello World!\n");
+static int dump(char *buf, long size) {
+	long i;
 
-	printval();
-	puts("overwrite variables.\n");
-	global_data = 0x20;
-	global_bss  = 0x30;
-	static_data = 0x40;
-	static_bss  = 0x50;
-	printval();
+	if (size < 0) {
+		puts("no data\n");
+		return -1;
+	}
 
-	while(1)
+	for (i = 0; i < size; i++) {
+		putxval(buf[i], 2);
+
+		if ((i & 0xf) == 15) {
+			puts("\n");
+		} else {
+			if ((i & 0xf) == 7) puts(" ");
+			puts(" ");
+		}
+	}
+	puts("\n");
+
+	return 0;
+}
+
+static void wait() {
+	volatile long i;
+	for (i = 0; i < 300000; i++)
 		;
+}
+
+int main(void) {
+	static char buf[16];
+	static long size = -1;
+	static unsigned char *loadbuf = NULL;
+	extern int buffer_start; // defined in linker script
+
+	init();
+
+	puts("k8os boot loader started.\n");
+	
+	while(1) {
+		puts("bootloader> ");
+		gets(buf);
+
+		if (!strcmp(buf, "load")) {
+			loadbuf = (char *)(&buffer_start);
+			size = xmodem_recv(loadbuf);
+			wait(); // wait for the xmodem file transfer app (minicom) termination to not lose the message
+			if (size < 0) {
+				puts("\nXMODEM receive error!\n");
+			} else {
+				puts("\nXMODEM receive succeeded.!\n");
+			}
+		} else if (!strcmp(buf, "dump")) {
+			puts("size: ");
+			putxval(size, 0);
+			puts("\n");
+			dump(loadbuf, size);
+		} else {
+			puts("unknown.\n");
+		}
+	}
 
 	return 0;
 }
